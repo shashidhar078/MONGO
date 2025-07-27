d7237aa1c524710bffc8bb9c410ecabc4c9906bb
shashidhar078 <nagunurishashidhar@gmail.com>
2025-07-27 22:41:39 +0530

    updated

--- book.js
+++ book.js
@@ -21,7 +21,35 @@ const bookSchema=new mongoose.Schema({
     },
     price:{
         type:Number,
+        min:1,
+    },
+    discount:{
+        type:Number,
+        default:0,
+    },
+    category:{
+        type:String,
+        enum:["fiction","non-fiction"],
     },
 });
 
 const Book=mongoose.model("Book",bookSchema);
+
+// let book1 = new Book({
+//     title:"Marvel Comic",
+//     author:"Stan lee",
+//     price:1200,
+//     category:"fiction",
+// });
+
+// book1.save().then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+Book.findByIdAndUpdate('68862932d97a9c7450f1eeb6',{price:1500},{runValidators:true}).then((res)=>{
+    console.log(res);
+}).catch((err)=>{
+    console.log(err);
+})
